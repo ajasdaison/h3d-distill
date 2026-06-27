84336f04cea3e65c20022ad8392fa41138195692
Ajas Daison <ajas.daison@gmail.com>
2026-06-27 13:52:15 +0530

    add web-distill

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
-    plugins: [
-        glsl()
-    ],
-    server: {
-        port: 3000,
-        open: "/examples/index.html"
-    }
+  plugins: [
+    glsl()
+  ],
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    open: "/examples/index.html",
+    allowedHosts: true
+  }
 });
